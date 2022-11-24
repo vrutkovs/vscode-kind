054aa24a9751f7f4be849ef381fcf5bba55e88ff
Vadim Rutkovsky <vrutkovs@redhat.com>
2022-11-24 09:30:22 +0000

    Fix build command

--- 02-build_kubelet.sh
+++ 02-build_kubelet.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 source settings.sh
 
-docker build -t ${image_name} -f custom-kubelet/Dockerfile custom-kubelet
+docker build --build-arg KUBERNETES_REPO="${KUBERNETES_REPO}" --build-arg KUBERNETES_REPO_COMMIT="${KUBERNETES_REPO_COMMIT}" -t ${image_name} -f custom-kubelet/Dockerfile custom-kubelet
 docker push ${image_name}

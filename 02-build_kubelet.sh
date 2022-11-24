e1822255422ee4f804ed0d5adbfd8857e0f69b19
Vadim Rutkovsky <vrutkovs@redhat.com>
2022-11-24 09:43:47 +0000

    Fix build-kubelet

--- 02-build_kubelet.sh
+++ 02-build_kubelet.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 source settings.sh
 
-docker build --build-arg KUBERNETES_REPO="${KUBERNETES_REPO}" --build-arg KUBERNETES_REPO_COMMIT="${KUBERNETES_REPO_COMMIT}" -t ${image_name} -f custom-kubelet/Dockerfile custom-kubelet
-docker push ${image_name}
+docker build --build-arg KUBERNETES_REPO="${KUBERNETES_REPO}" --build-arg KUBERNETES_REPO_COMMIT="${KUBERNETES_REPO_COMMIT}" -t "${local_image_name}" -f custom-kubelet/Dockerfile custom-kubelet
+docker push "${local_image_name}"
--- settings.sh
+++ settings.sh
@@ -5,4 +5,5 @@ reg_port='5000'
 
 KUBERNETES_REPO="https://github.com/vrutkovs/kubernetes"
 KUBERNETES_REPO_COMMIT="872e5269135"
+local_image_name="localhost:${reg_port}/node:${KUBERNETES_REPO_COMMIT}"
 image_name="${reg_name}:${reg_port}/node:${KUBERNETES_REPO_COMMIT}"
